937583971b330c2c4ec5ecfeeb424a0ffa0e7133
jahnvi <jahnvi@thewebsitefactory.london>
2023-10-12 23:18:09 +0530

    create api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import classes from './App.module.css';
-import ProductPreview from './ProductPreview';
-import ProductDetail from './ProductDetail';
-import Topbar from './Topbar';
+// import classes from './App.module.css';
+// import ProductPreview from './ProductPreview';
+// import ProductDetail from './ProductDetail';
+// import Topbar from './Topbar';
+
+import ComA from './ComA';
 
 function App() {
   return (
     <div className="App">
-      <Topbar />
+      {/* <Topbar />
 
       <div className={classes.MainContainer}>
         <div className={classes.ProductPreview}>
@@ -17,7 +19,9 @@ function App() {
         <div className={classes.ProductData}>
           <ProductDetail />
         </div>
-      </div>
+      </div> */}
+
+      <ComA />
     </div>
   )
 
